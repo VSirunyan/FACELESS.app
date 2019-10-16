683f77582c5a44ad62508fe43ac4003ae141dd76
matos <matos@ispras.ru>
2019-10-17 00:20:36 +0400

    dded postgresql configurations.

--- faceless/settings.py
+++ faceless/settings.py
@@ -78,10 +78,15 @@ WSGI_APPLICATION = 'faceless.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'wifis',
+        'USER': 'postgres',
+        'PASSWORD': 'llvm1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
